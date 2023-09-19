bfd5ba83886d99fa7c1edd6e6735630d668b4bd4
Moeletsi Nkuna <jasonnkuna@gmail.com>
2023-09-20 01:31:15 +0200

    version 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const deleteScreenVal = document.getElementById('delete');
 const screenVal =document.getElementsByClassName('screen');
 const screenColor = document.getElementsByClassName('screenColor');
+const allButtons = document.getElementsByClassName("row-1");
+let jay ;
+let dino = document.getElementById("dino");
 let sine = document.getElementById('sine');
 let result = document.getElementById('result')
 let answer;
@@ -20,6 +23,8 @@ let pie = entity.pie.value;
 let state = true;
 
 
+screenColor.mode.style.borderRadius ="10px";
+screenColor.mode.style.backgroundColor ="rgb(161, 235, 161)";
 //deleting one charater on the screen 
   deleteScreenVal.addEventListener('click', function(){
   screenVal.onScreen.value =  screenVal.onScreen.value.slice(0,-1);
@@ -28,42 +33,37 @@ let state = true;
     screenColor.mode.addEventListener('click', function(){
     state = !state;
     screenVal.onScreen.style.backgroundColor =  state? "rgb(161, 235, 161)" : "black";
+    jay = "undefined";
      //if the state is false the calculator is off
- 
     if(state === false){
-      console.log("the calculator is off");
       screenVal.onScreen.value = "";
       }else{
-        console.log("the calculator is on");
-        screenVal.onScreen.value = "";
+        
         }
   })
-  //calculaations
+  //calculations only when the eqaul button is clicked
   result.addEventListener('click', function(){
 
     userInput = screenVal.onScreen.value; // user input
     // replacing pie symbol with it's value
-    try {
-      userInput = userInput.replaceAll(pie, `${Math.PI.toFixed(3)}`);
-    } catch (error) {
-      console.log("failed");
-    }
-    //search for pie
-   
-    //searching for sine, cose and pow functions
+    
+    userInput = userInput.replaceAll(pie, `${Math.PI.toFixed(3)}`);
+
+    // replacing "Ans" with the previous answer
+
+    userInput = userInput.replaceAll("Ans", `${jay}`);
+  
+    //searching for sine, cose , tan and pow functions
     for(i=0; i<func.length; i++){
-      do{
         
+       do{
         funcIndex = userInput.search(func[i]);
+        console.log(funcIndex);
     // if sin || cos || tan || pow is part of the input
         if(funcIndex !=-1){
-              console.log(userInput);
-              console.log(`${func[i]} found`);
               rightBrace = userInput.indexOf(")",funcIndex);
               Angle = userInput.substring(funcIndex+4, rightBrace); //getting the angele insie the ()
               funcVar =  userInput.substring(funcIndex, rightBrace+1); 
-              console.log(funcVar);
-              console.log(Angle);
 
               if(func[i] === "sin"){
                 funcResult =  Math.sin(Angle*Math.PI/180);
@@ -73,33 +73,27 @@ let state = true;
                         funcResult =  Math.tan(Angle*Math.PI/180);
                          }else if(func[i] === "pow"){
                            comm = Angle.indexOf(',');
-                           console.log(comm);
                            x = Angle.substring(0, comm);
                            y = Angle.substring(comm+1);
                            x = parseInt(x);
                            y = parseInt(y);
                            funcResult =  Math.pow(x, y);
-                           console.log(x,y);                        
                             }else if(func[i] == "log"){
-                               console.log("log is found");
                                funcResult =  Math.log(Angle);
                              }
             
               funcResult = funcResult.toFixed(3);
-              console.log(funcResult);
               userInput = userInput.replace(funcVar, funcResult);
-              console.log(userInput);
           }//end of if statement
         }while(funcIndex !=-1);
     }//end of for loop
-
+   
     //searching for power ^   and doing the calculations if it exists
     expIndex = userInput.indexOf("^");
     if(expIndex != -1){
         base = userInput.substring(0, expIndex);
         exponent = userInput.substring(expIndex+1);
         userInput = base;
-        console.log(userInput);
         if(exponent == 0){
             userInput = 1;
              }else if(exponent == 1){
@@ -111,6 +105,18 @@ let state = true;
                    }
                      }
      }
-    answer = eval(userInput);
-    screenVal.onScreen.value = answer;
+
+      answer = eval(userInput);
+      jay = answer;
+      screenVal.onScreen.value = answer;  
 })
+
+  dino.onclick = function(){
+  screenVal.onScreen.value += "Ans";
+}
+
+function removeAnswer() {
+  screenVal.onScreen.classList.remove("answer");
+} 
+
+ 
